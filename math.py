cc1da81c97434f4d0ca22a6ade00155bcb1437e3
srisakamuri <81152253+srisakamuri@users.noreply.github.com>
2021-03-22 15:48:55 +0530

    Add files via upload

---
+++ math.py
@@ -0,0 +1,18 @@
+#add implementation
+def add(x,y):
+    return x+y
+#substract implementation
+def subtract(x,y):
+    return x-y
+#multiply implementation
+def multiply(x,y):
+    return x*y
+#divide implementation
+def divide(x,y):
+    if(x==0):
+      return Divide_by_0_error
+    else:
+       return x/y
+def sqaure(x):
+    returnx*x
+
